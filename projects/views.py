a066080a8bd03a56a47d33d3d287aebdd7144220
shelleybehen <shelleybehen@gmail.com>
2021-11-14 17:24:45 +1000

    Added profile detail to views

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework.views import APIView 
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, ProfileDetailSerializer
 
 class ProfileList(APIView):
 
@@ -16,3 +18,12 @@ class ProfileList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        return Profile.objects.get(pk=pk)
+        
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
